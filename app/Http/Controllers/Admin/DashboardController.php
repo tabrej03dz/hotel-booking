494f1b97063db4efef86aacc868391448363e4f5
tabrej03dz <tabrejalam770496@gmail.com>
2025-05-24 17:01:30 +0530

    making dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
+         $totalBookings = Booking::count();
+    $totalPayments = Payment::sum('amount');
+    $completedBookings = Booking::where('status', 'completed')->count();
+    $pendingPayments = Payment::where('status', 'pending')->sum('amount');
 
-        return view('dashboard');
+        return view('dashboard', compact('totalBookings', 'totalPayments', 'completedBookings', 'pendingPayments'));
     }
 }
